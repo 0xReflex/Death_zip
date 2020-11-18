04fd2a25b76407931e75a51a777ced4b5be8a6d6
unknown <prrprrbro23@gmail.com>
2020-11-18 18:52:48 +0530

    second commit minor changes

--- death_zip.py
+++ death_zip.py
@@ -5,6 +5,7 @@ import shutil
 os.mkdir("0")
 for i in range(1):
 	file = open("0/%i" %i, "w")
-	file.write("0" * 1024 * 1024 * 500)
-
+	file.write("0" * 1024 * 1024 * 1)
+	for folders in range(1):
+		
 file.close()
